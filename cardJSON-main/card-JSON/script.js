5af6e0f41b383d9bdaecba29452428c39183d3aa
joaoCerqueira2010 <joao.cerqueira.nascimento@escola.pr.gov.br>
2026-05-06 12:10:47 -0300

    Add files via upload

--- cardJSON-main/card-JSON/script.js
+++ cardJSON-main/card-JSON/script.js
@@ -1,28 +1,35 @@
-sync  function cafe(){
-const destino = document.getElementById('destino');
+async function cafe(){
+  const destino = document.getElementById_('destino');
 
   try {
-
-
     const response = await fetch('card.json');
+
+    if (!response.ok) throw new Error('Falha ao carregar dados');
+
+    const cafes = await response.json();
+
+    destino.innerHTML = '';
+
+    cafes.forEach(item => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      card.innerHTML = `
+      <img src="img/${item.imagem}" alt="Foto de ${item.nome}"
+      <h3></h3>
+      <p></p>
+      <a href="${item.endereco}">
+        <button type="button">Saiba Mais</button>
+      </a>
+      `;
+
+      card.querySelector('h3').textContent = item.nome;
+      card.querySelector('p').textContent = item.descricao;
+
+      destino.appendChild(card);
+    });
+  } catch (error) {
+    console.error('Erro na requisição', error);
+    destino.innerHTML = '<p>Desculpe, não foi possivel carregar as informações no momento. </p>;';
   }
-  if (!response.ok) throw new Error('falha no carregamento de dados !!')
-
-  const cafes = await response.json();
-
-
-
-
-
-  cafes.forEach(item => {
-    const card = document.createElement('div')
-    card.className = 'card';
- card.innerHTML = ´
- <img src="/${item.imagem}" alt="foto de $(item.nome">
- <h3></h3>
- <P></P>
-<a   href="img/${item.endereco}">
-
-<button type="button">SAIBA MAIS</button>
-</a>
-´;
+}
